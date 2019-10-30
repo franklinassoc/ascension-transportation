c2bb4d5dde73c4e30fd39e8546092407fa41143e
James Taylor <35105264+jtatfranklin@users.noreply.github.com>
2019-10-30 17:37:24 -0500

    update google-doc-url.js
    check name of shared google sheet

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,3 +1,2 @@
 // target is Ascension - Leaflet Maps with Google Sheets
-//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ct_P6HyAiMwYxb1n0lm7cOOojVT-G3BT8rXZgrvje_E/edit#gid=0';
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1EgQA2K5nJbCKXP3WugnZ_zUI8QU4xHqafDyxNTWf2bw/edit#gid=282680176';
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,8 +1,8 @@
 var constants = {
   // Tabs in Google Sheet
   notesSheetName: 'Notes',
-	optionsSheetName: 'Options',
-	pointsSheetName: 'Points',
+  optionsSheetName: 'Options',
+  pointsSheetName: 'Points',
   polygonsSheetName: 'Polygons',
   polylinesSheetName: 'Polylines',
   /* OPTIONS */
